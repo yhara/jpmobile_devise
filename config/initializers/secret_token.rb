3a049a762081865fa7a8ca2f1830c45632ac4dba
Yutaka HARA <yutaka.hara+github@gmail.com>
2010-10-25 23:33:27 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JpmobileDevise::Application.config.secret_token = 'e161e842af8a9688fb993be3635203a56fbbf23c6fb9d1f3aede22f73a1f1c4293841bbb0ce9102bb901c79b5cb930d64e48265e96b8c721a65ae36d280ca0d9'
